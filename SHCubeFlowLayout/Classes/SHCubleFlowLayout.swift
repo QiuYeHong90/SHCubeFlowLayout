e4d03c98cebc241d77612981741448b43b90b4c5
YeQiu <793983383@qq.com>
2024-11-12 16:43:10 +0800

    fix code

--- SHCubeFlowLayout/Classes/SHCubleFlowLayout.swift
+++ SHCubeFlowLayout/Classes/SHCubleFlowLayout.swift
@@ -7,8 +7,7 @@
 
 import UIKit
 
-open class SHCubleFlowLayout: UICollectionViewLayout {
-    open var itemSize: CGSize = .zero
+open class SHCubleFlowLayout: UICollectionViewFlowLayout {
     
     open override var collectionViewContentSize: CGSize {
         let containerW = self.collectionView?.frame.size.width ?? 0
@@ -22,8 +21,33 @@ open class SHCubleFlowLayout: UICollectionViewLayout {
         return true
     }
     open override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-       
+        
         let attributtes = UICollectionViewLayoutAttributes.init(forCellWith: indexPath)
+        attributtes.configAttributes(collectionView: self.collectionView, itemSize: self.itemSize)
+        return attributtes
+    }
+    open override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+//        var arr = super.layoutAttributesForElements(in: rect)
+//        if let arr = arr,arr.count > 0 {
+//            for item in arr {
+//                item.configAttributes(collectionView: self.collectionView, itemSize: self.itemSize)
+//            }
+//            return arr
+//        }
+        let rowCount = self.collectionView?.numberOfItems(inSection: 0) ?? 0
+        var newArray: [UICollectionViewLayoutAttributes] = []
+        for item in 0..<rowCount {
+            let indexPath = IndexPath.init(row: item, section: 0)
+            if let attributes = self.layoutAttributesForItem(at: indexPath) {
+                newArray.append(attributes)
+            }
+        }
+        return newArray
+    }
+    
+    func configAttributes(attibutues: UICollectionViewLayoutAttributes) {
+        let indexPath = attibutues.indexPath
+        var attributtes = attibutues
         let containerW = self.collectionView?.frame.size.width ?? 0
         let containerH = self.collectionView?.frame.size.height ?? 0
         let x: CGFloat = CGFloat(self.collectionView?.contentOffset.x ?? 0)
@@ -51,21 +75,41 @@ open class SHCubleFlowLayout: UICollectionViewLayout {
         let position = (x) / self.itemSize.width
         print("position---\(x)")
         attributtes.zIndex = allCount - abs(indexPath.row - Int(position))
-        return attributtes
     }
-    open override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        let arr = super.layoutAttributesForElements(in: rect)
-        if let arr = arr,arr.count > 0 {
-            return arr
-        }
-        let rowCount = self.collectionView?.numberOfItems(inSection: 0) ?? 0
-        var newArray: [UICollectionViewLayoutAttributes] = []
-        for item in 0..<rowCount {
-            let indexPath = IndexPath.init(row: item, section: 0)
-            if let attributes = self.layoutAttributesForItem(at: indexPath) {
-                newArray.append(attributes)
-            }
-        }
-        return newArray
+}
+
+extension UICollectionViewLayoutAttributes {
+    func configAttributes(collectionView: UICollectionView?, itemSize: CGSize) {
+        
+        var attributtes = self
+        let indexPath = attributtes.indexPath
+        let containerW = collectionView?.frame.size.width ?? 0
+        let containerH = collectionView?.frame.size.height ?? 0
+        let x: CGFloat = CGFloat(collectionView?.contentOffset.x ?? 0)
+        let arc = CGFloat.pi * 2
+        let rowCount = 4
+        let allCount = collectionView?.numberOfItems(inSection: 0) ?? 0
+        attributtes.center = .init(x: x + containerW * 0.5, y: containerH * 0.5)
+//        attributtes.size = .init(width: self.itemSize.width - 100, height: self.itemSize.height - 213)
+        attributtes.size = .init(width: itemSize.width, height: itemSize.height)
+        var transform = CATransform3DIdentity;
+        transform.m34 = -1 / 700;
+        let radius = attributtes.size.width / 2 / tan(arc / 2.0 / CGFloat(rowCount) )
+        let angle = CGFloat(indexPath.row % 4) * arc / 4 - ((x/containerW) + 1) / CGFloat(rowCount) * arc + 1 / CGFloat(rowCount) * arc
+        
+        
+        transform = CATransform3DRotate(transform, angle, 0, 1, 0)
+        print("radius ---\(radius)")
+        let rate: CGFloat = 0.785
+        transform = CATransform3DTranslate(transform, 0, 0, radius * rate)
+        
+        transform = CATransform3DScale(transform, 1 * rate , 1 * rate,1)
+        attributtes.transform3D = transform
+        
+        let ruler = collectionView?.bounds.midX ?? 0
+        let position = (x) / self.size.width
+        print("position---\(x)")
+        attributtes.zIndex = allCount - abs(indexPath.row - Int(position))
+        print("attributtes.zIndex === \(attributtes.zIndex)")
     }
 }
